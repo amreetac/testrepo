4cf9689d0b97161efac1631fa98979ebe96b35eb
Amreeta Choudhury <amreeta89@yahoo.com>
2024-07-10 16:07:51 -0400

    Add files via upload
    AI Applications with LLMs

---
+++ image_captioning_app.py
@@ -0,0 +1,42 @@
+import gradio as gr
+import numpy as np
+from PIL import Image
+from transformers import AutoProcessor, BlipForConditionalGeneration
+
+# Load the pretrained processor and model
+processor = AutoProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
+model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base")
+
+def caption_image(input_image: np.ndarray):
+    # Convert numpy array to PIL Image and convert to RGB
+    raw_image = Image.fromarray(input_image).convert('RGB')
+    
+    # Process the image
+    # Load your image, DONT FORGET TO WRITE YOUR IMAGE NAME
+    img_path = "dog.jpg"
+# convert it into an RGB format 
+    image = Image.open(img_path).convert('RGB')
+
+# You do not need a question for image captioning
+    text = "the image of"
+    inputs = processor(images=image, text=text, return_tensors="pt")
+
+
+    # Generate a caption for the image
+    outputs = model.generate(**inputs, max_length=50)
+
+
+    # Decode the generated tokens to text and store it into `caption`
+    caption = processor.decode(outputs[0], skip_special_tokens=True)
+
+    return caption
+
+iface = gr.Interface(
+    fn=caption_image, 
+    inputs=gr.Image(), 
+    outputs="text",
+    title="Image Captioning",
+    description="This is a simple web app for generating captions for images using a trained model."
+)
+
+iface.launch()
